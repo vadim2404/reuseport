c1f45c484c6bb844dc1f2a60a9487f2bf3c78f74
Vadim Kharitonov <vadim@neon.tech>
2023-01-20 16:02:19 +0100

    Don't be overflowed with finished tasks

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,19 @@ use futures::future::join_all;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpSocket};
 use tokio::signal::unix::{signal, SignalKind};
+use tokio::sync::Mutex;
+use tokio::task::JoinHandle;
+use tokio::time::{sleep, Duration};
 use tokio_util::sync::CancellationToken;
 
 use std::env;
 use std::error::Error;
 use std::process;
 use std::sync::Arc;
+
+const MAX_LISTENERS: u32 = 1024;
+const BUFFER_SIZE: usize = 1024;
+const CLEAN_INTERVAL: u64 = 5;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -28,7 +35,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     socket.set_reuseaddr(true)?;
     socket.set_reuseport(true)?;
     socket.bind(addr.parse()?)?;
-    let listener = socket.listen(1024)?;
+    let listener = socket.listen(MAX_LISTENERS)?;
     println!("Process {pid} listening on: {}", addr);
 
     let mut signal_stream = signal(SignalKind::hangup())?;
@@ -53,13 +60,54 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+#[derive(Default)]
+struct Tasks {
+    tasks: Vec<JoinHandle<()>>,
+}
+
+impl Tasks {
+    fn add(&mut self, task: JoinHandle<()>) {
+        self.tasks.push(task);
+    }
+
+    async fn join_all(&mut self) {
+        join_all(self.tasks.drain(..)).await;
+    }
+
+    fn remove_finished_tasks(&mut self) {
+        self.tasks.retain(|task| !task.is_finished());
+    }
+
+    fn len(&self) -> usize {
+        self.tasks.len()
+    }
+}
+
 async fn handle_connection(
     pid: u32,
     listener: TcpListener,
     postfix: String,
     cancellation_token: CancellationToken,
 ) -> Result<(), Box<dyn Error>> {
-    let mut tasks = vec![];
+    let tasks = Arc::new(Mutex::new(Tasks::default()));
+
+    let cancellation_token_clone = cancellation_token.clone();
+    let tasks_clone = Arc::clone(&tasks);
+    let remove_finish_tasks = tokio::spawn(async move {
+        loop {
+            tokio::select! {
+                _ = cancellation_token_clone.cancelled() => {
+                    break;
+                }
+
+                _ = sleep(Duration::from_secs(CLEAN_INTERVAL)) => {
+                    let mut tasks = tasks_clone.lock().await;
+                    tasks.remove_finished_tasks();
+                }
+            }
+        }
+    });
+
     let postfix = Arc::new(postfix);
 
     loop {
@@ -69,18 +117,22 @@ async fn handle_connection(
                 drop(listener);
 
                 // wait until all other connections are closed
+                let _ = remove_finish_tasks.await;
+
+                let mut tasks = tasks.lock().await;
                 println!("process {pid} closing tasks: {}", tasks.len());
-                join_all(tasks).await;
+                tasks.join_all().await;
                 break;
             }
 
             Ok((mut socket, addr)) = listener.accept() => {
                 println!("Process {pid} accepted connection from: {addr}");
 
-                let postfix_clone = postfix.clone();
+                let postfix_clone = Arc::clone(&postfix);
 
-                tasks.push(tokio::spawn(async move {
-                    let mut buf = BytesMut::with_capacity(1024);
+                let mut tasks = tasks.lock().await;
+                tasks.add(tokio::spawn(async move {
+                    let mut buf = BytesMut::with_capacity(BUFFER_SIZE);
 
                     loop {
                         let n = socket
@@ -92,7 +144,7 @@ async fn handle_connection(
                             break;
                         }
 
-                        buf.put(&postfix_clone.as_bytes()[..]);
+                        buf.put(postfix_clone.as_bytes());
 
                         socket
                             .write_buf(&mut buf)
